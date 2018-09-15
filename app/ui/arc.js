310c24a2a955ea37f20f957c9ce34629732f54df
Merciyah <lucasmerciyah@Lucass-MacBook-Pro.local>
2018-09-15 14:16:52 +0100

    Init Bit_Map

--- app/ui/arc.js
+++ app/ui/arc.js
@@ -1,47 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View, } from 'react-native';
-import {Navigator} from 'react-native-deprecated-custom-components'
-
-// Our Views/Components or whatever you want to call em
-
-import BattleView from './battle'
-import Splash from './splash'
-import CharacterSelection from './charactersSelection'
+import Navigator from './navigationAi'
 
 
-// Usable routes
-const routes = {
-  splash: Splash,
-  characterSelection: CharacterSelection,
-  battleview: BattleView
-};
-
-
-// This is our arc view as well as our Navigation unit
-// Dear React Native at facebook. Please stop screwing with our Navigation systems.
-
-
-export default class Index extends React.Component {
-  constructor(){
-    super()
-  }
-
-  // define scene
-  renderScene({id}, navigator){
-    const Scene = routes[id]
-    return <Scene {...this.props} hideMenu={() => this.hideMenu()} pushToCache = {this.pushToCash} navigator={navigator}/>
-  }
-
-
-  render() {
-    return (
-    <Navigator
-              style={{flex: 1}}
-              ref={'NAV'}
-              initialRoute={{id: 'characterSelection', name: 'characterSelection'}}
-              renderScene={this.renderScene.bind(this)}
-            /> 
-    );
+export default class Arc extends React.Component {
+  render(){
+    return (<View style={{flex:1}}><Navigator /></View>);
   }
 }
 
--- app/ui/charactersSelection.js
+++ app/ui/charactersSelection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, ListView, Image, ImageBackground, TouchableOpacity, Text, View } from 'react-native';
 import characters from '../math/data_map/characters'
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 
 //This could potentially contain a lot of state data and logic methodology
@@ -8,6 +9,8 @@ import characters from '../math/data_map/characters'
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 import el7 from "../elements/images/e7.png"
+var blurb_count = 0;
+var blurbs = ['info1', 'info2', 'info3'];
 
 export default class CharacterSelection extends React.Component {
   // Do we want characters displayed in a listView? lets hace an indie view for now OK
@@ -15,38 +18,69 @@ export default class CharacterSelection extends React.Component {
     super(props)
 
     this.state = {
-      characters: null,
-      characterAvy: false,
-      imageData: el7,
-      name:'Hello World',
+      isCharacters: false,
+      blurb_count: blurb_count, 
+      activeUnit: characters[0],
       dataSource : ds.cloneWithRows(characters)
     }
+
   }
 
   selectedChatacters(){
     // This returns an array
   }
   eachCharacter(data){
+    return(<TouchableOpacity onPress = {() => this.setState({activeUnit: data, isCharacter: true})} style={{backgroundColor:'#d3d3d3', margin:5, borderRadius:5}}><Image source ={data.image} resizeMode="contain" style={{width:50, height:70}} /></TouchableOpacity>)
+  }
 
-    return(<TouchableOpacity onPress = {() => this.setState({imageData: data.image})} style={{backgroundColor:'#d3d3d3', margin:5, borderRadius:5}}><Image source ={data.image} resizeMode="contain" style={{width:50, height:70}} /></TouchableOpacity>)
+
+  queueMoves(){
+
+  }
+
+  _nextBlurb(){
+    //I want to be able to cycle this. Since we know each character has 3 blurbs, we'll only need to add a blurb_count at 3 = 0 
+    blurb_count = blurb_count+1
+    if(blurb_count == 3){
+      blurb_count = 0
+    }
+    this.setState({
+      blurb_count: blurb_count
+    })
+  }
+
+  movesList(){
+
+  }
+
+  expandibleMovesListButton(){
+    return(<TouchableOpacity style={{width:70, height:70, borderRadius:35, backgroundColor:''}}></TouchableOpacity>)
   }
 
   selectedChar(){
-    if(this.state.imageData != null){
+    if(this.state.activeUnit.image != null){
         return(<Image source={this.state.dataImage} resizeMode="stretch" style={{flex:1, width:null, height:null}} />)
       }else{
         return(<View />)
       }}
 
+      moreJokes(){
+        return(<TouchableOpacity onPress={() => this._nextBlurb()} style={{ position:'absolute', right:10, bottom:5, width:40, height:40, justifyContent:'center', alignItems:'center'}}>
+      <Icon name = 'angle-double-right'  size = {27} color ="#333" />
+       </TouchableOpacity>)
+      }
 
   render(){
     return(
     <View style={{flex:1}}>
       <View style={{flex:1, flexDirection:'row'}}>
       <View style={{flex:4}}>
-      <ImageBackground source ={this.state.imageData} resizeMode="contain" style={{flex:1, width:null, height:null}}>
-      <View style={{backgroundColor:'rgba(0,0,0,0.4)', width:100, padding:5}}>
-      <Text style={{color:'#fff'}}>{this.state.name}</Text>
+      <ImageBackground source ={this.state.activeUnit.image} resizeMode="contain" style={{flex:1, width:null, height:null}}>
+      <View style={{backgroundColor:'rgba(0,0,0,0.6)', marginTop:100, width:70, padding:5}}>
+      <Text style={{color:'#fff', fontSize:7, fontWeight:'600'}}>{this.state.activeUnit.first_name.toUpperCase()}</Text>
+      </View>
+      <View style={{position:'absolute', bottom:0, padding:10, margin:5, backgroundColor:'rgba(0,0,0,0.6)', alignSelf:'center'}}>
+      <Text style={{color:'#fff'}}>{this.state.activeUnit.background.info1}</Text>
       </View>
       </ImageBackground>
       </View>
@@ -57,6 +91,7 @@ export default class CharacterSelection extends React.Component {
         style= {{flex:1}}
         />
         </View>
+        {this.moreJokes()}
         </View>
         );
       }
