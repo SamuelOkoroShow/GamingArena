3a11ce07f6a7bea07b301a70ca24be5bcdbd5ece
Merciyah <lucasmerciyah@Lucass-MacBook-Pro.local>
2018-09-22 09:39:16 +0100

    Check

--- app/ui/rival.js
+++ app/ui/rival.js
@@ -6,25 +6,19 @@ import back1 from "../elements/images/background1.jpg"
 import colors from '../math/bit_map/colors'
 import { StyleSheet, Image, Text, View, StatusBar, ImageBackground } from 'react-native';
 
-//Firebase
-import * as firebase from 'firebase';
-import fireConfig from '../../auth'
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: fireConfig.apiKey,
-  authDomain: fireConfig.authDomain,
-  databaseURL: fireConfig.databaseURL,
-  storageBucket: fireConfig.storageBucket,
-};
-
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
+//Link to firebase
 
 //This could potentially contain a lot of state data and logic methodology
 // We're going to try to keep this as clean as possible 
 
 export default class Rival extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
   render() {
     return (<ImageBackground source ={rival3} resizeMode = "stretch" style = {{flex:1, height:null, width:null}}>
       </ImageBackground>);
